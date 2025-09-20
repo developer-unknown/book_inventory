d6f16a2d78c1ea2ca8d2ec22771843d6cb0c9a02
developer-unknown <cardenashernandezmarcos14@gmail.com>
2025-09-19 21:23:22 -0600

    Configuracion de BD y API

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { BookModule } from './book/book.module';
+import { TechnicalSheetModule } from './technical-sheet/technical-sheet.module';
+import { Book } from './book/entities/book.entity';
+import { TechnicalSheet } from './technical-sheet/entities/technical-sheet.entity';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    BookModule,
+    TechnicalSheetModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: 3306,
+      database: process.env.DB_DATABASE,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      entities: [Book, TechnicalSheet],
+      synchronize: true,
+    }),
+  ],
   controllers: [],
   providers: [],
 })
